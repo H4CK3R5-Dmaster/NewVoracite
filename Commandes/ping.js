57878b092fa3cb4abc3c15e2ddc254e523bd876e
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-12-31 14:07:41 +0100

    feat: adding slash

--- Commandes/ping.js
+++ Commandes/ping.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js")
 
 module.exports = {
     name:"ping",
+    description: "connaître la latence du bot",
+    permission: "Aucune",
+    dm: true,
     async run(bot, message){
         await message.reply(`Ping : \`${bot.ws.ping}\``)
     }

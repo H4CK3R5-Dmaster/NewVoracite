d87081b7b38c2682c5ade95949619d3c97c9d389
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-12-31 11:51:26 +0100

    feat: ping

---
+++ Loaders/loadCommands.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+module.exports = async bot => {
+    fs.readdirSync("./Commandes").filter(f => f.endsWith(".js")).forEach(async file => {
+
+        let command = require(`../Commandes/${file}`)
+        if(!command.name || typeof command.name !== "string") throw new TypeError(`Commande ${file.slice(0, file.length - 3)} pas de nom`)
+        bot.commands.set(command.name, command)
+        console.log(`[commandes] - [+] ${file} chargé`)
+    })
+}
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client({intents: 3276799})
+const intents = new Discord.IntentsBitField(3276799)
+const bot = new Discord.Client({intents})
+const loadCommands = require("./Loaders/loadCommands")
 const config = require('./config.json')
+
+bot.commands = new Discord.Collection()
+
+loadCommands(bot)
+
+bot.on("messageCreate", async message => {
+    if(message.content == "-ping") return bot.commands.get("ping").run(bot, message)
+})
 
 bot.on("ready", async () => {
     console.log(`Logged at ${bot.user.tag} !`)
